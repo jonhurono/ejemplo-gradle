b3dd3070a0ed7468afae63b3681f9123248135ca
John Chaves <jjchavescc@gmail.com>
2022-01-24 22:12:13 -0300

    modificación para error

--- gradle.groovy
+++ gradle.groovy
@@ -8,24 +8,28 @@ def call(){
   
     stage('Build & Unit Test') {
         sh 'env'
+        STAGE = env.STAGE
         println "Stage: ${env.STAGE_NAME}"
         sh './gradlew clean build'
     }
 
     stage('Sonar') {
+        STAGE = env.STAGE
         println "Stage: ${env.STAGE_NAME}"
-        ...
     }
 
     stage('Run') {
+        STAGE = env.STAGE
         println "Stage: ${env.STAGE_NAME}"
     }
 
     stage('Test') {
+        STAGE = env.STAGE
         println "Stage: ${env.STAGE_NAME}"
         }
 
 	stage('Nexus') {
+        STAGE = env.STAGE
         println "Stage: ${env.STAGE_NAME}"
     }
     
